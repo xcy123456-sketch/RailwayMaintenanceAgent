1af5d28c3ad04a3822544131c4329b00a9aaa66e
徐淙洋 <xucongyang@xucongyangdeMacBook-Air.local>
2026-03-01 19:28:43 +0800

    1

--- FileReadingTool/pdf_reader/pdf_reader.py
+++ FileReadingTool/pdf_reader/pdf_reader.py
@@ -1,6 +1,7 @@
 import os
 import pdfplumber
 from pathlib import Path
+import pandas as pd
 
 def get_all_pdfs(folder_path)->list:
     '''
@@ -25,12 +26,43 @@ if not os.path.exists(result_table_dir):
 pdf_file_path = os.path.join(current_dir, 'file_lib')
 pdf_list = get_all_pdfs(pdf_file_path)
 
-for pdf in pdf_list:
-    file_dir = os.path.join(pdf_file_path, pdf.name)
-    with pdfplumber.open(file_dir) as pdf:
-        for page in pdf.pages:
-            if page.extract_tables():
-                print(page.extract_tables())
+# 测试
+file_dir = os.path.join(pdf_file_path, "《高速铁路线路维修规则》（铁工电〔2023〕106号）.pdf")
+
+with pdfplumber.open(file_dir) as pdf:
+    for page_num in range(pdf.pages.__len__()):
+        page_n = pdf.pages[page_num] # 第一页
+        tables = page_n.find_tables()
+        for i, table in enumerate(tables):
+            bbox = table.bbox
+            # 裁剪表格区域
+            cropped_page = page_n.crop([bbox[0], bbox[1]-50, bbox[2], bbox[3]])
+
+            # 转成图片
+            img = cropped_page.to_image(resolution=300)
+
+            # 单独保存
+            img.save(os.path.join(current_dir, "result/table", f"table_{page_num}_{i}.png"))
 
 
+    # page_table_list = page_n.extract_tables()
+    # for table_idx in range(page_table_list.__len__()):
+    #     table = page_table_list[table_idx]
+    #     print(table)
+    #     table_df = pd.DataFrame(table)
+    #     table_df = table_df.apply(
+    #         lambda col: col.str.replace(r"\s+", "", regex=True))        
+    #     table_df.to_csv(
+    #         os.path.join(current_dir, 'result', 'table',  f"table_{page_num}_{table_idx}.csv"))
+        
+
+# for pdf in pdf_list:
+#     file_dir = os.path.join(pdf_file_path, pdf.name)
+#     with pdfplumber.open(file_dir) as pdf:
+#         for page in pdf.pages:
+#             if page.extract_tables():
+#                 print(page.extract_tables())
+
+# if __name__=="__main__":
+#     # 一下wz
 
--- FileReadingTool/pdf_reader/test.py
+++ FileReadingTool/pdf_reader/test.py
@@ -1,4 +1,32 @@
+import base64
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+import dotenv
+import os
 
-def clean_line(s: str) -> str:
-    return " ".join(s.split()).strip()
-print(clean_line('\n djsbfwfbubf'))
+os.environ["DASHSCOPE_API_KEY"] = dotenv.get_key(dotenv.find_dotenv(), "DASHSCOPE_API_KEY")
+
+def to_data_url(path: str) -> str:
+    ext = path.split(".")[-1].lower()
+    mime = "jpeg" if ext in ["jpg", "jpeg"] else ext
+    with open(path, "rb") as f:
+        b64 = base64.b64encode(f.read()).decode("utf-8")
+    return f"data:image/{mime};base64,{b64}"
+
+llm = ChatOpenAI(
+    model="qwen-vl-plus",
+    api_key='sk-f6cf4f3ccd6649bda55450d7197b8232',
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+)
+
+img_data_url = to_data_url("/Users/xucongyang/Desktop/python_projects/RailwayMaintenanceAgent/FileReadingTool/pdf_reader/result/table/table_14_1.png")
+
+msg = HumanMessage(
+    content=[
+        {"type": "image_url", "image_url": {"url": img_data_url}},
+        {"type": "text", "text": "这张图里有什么？给出关键目标与文字说明。"},
+    ]
+)
+
+print(llm.invoke([msg]).content)
